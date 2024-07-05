f26a964d8e9813bd900a8de2f4ffdbe23ca6a387
Kyriakos <kyriakos.bakirlis@gmail.com>
2024-07-05 20:48:31 +0200

    Completing day 7 part 1

---
+++ 7_1/main.go
@@ -0,0 +1,222 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	hands := getHands()
+	sorted := sortHands(hands)
+
+	fmt.Println(sorted)
+	sum := 0
+	for i, hand := range sorted {
+		sum += (i + 1) * hand.bid
+	}
+	fmt.Println(sum)
+}
+
+func sortHands(hands []Hand) []Hand {
+	sort.Slice(hands, func(i, j int) bool {
+		return isHandStronger(hands[j], hands[i])
+	})
+	return hands
+}
+
+func getHands() []Hand {
+	filePath := "input.txt"
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result := []Hand{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		bid, err := strconv.Atoi(fields[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		result = append(result, Hand{fields[0], bid})
+
+	}
+	return result
+}
+
+// Returns true if handA is stronger
+func isHandStronger(handA Hand, handB Hand) bool {
+	handAStrength := handA.getHandStength()
+	handBStrength := handB.getHandStength()
+
+	if handAStrength > handBStrength {
+		return true
+	} else if handAStrength < handBStrength {
+		return false
+	} else {
+		for i := range handA.cards {
+			if getCardValue(handA.cards[i]) > getCardValue(handB.cards[i]) {
+				return true
+			}
+			if getCardValue(handA.cards[i]) < getCardValue(handB.cards[i]) {
+				return false
+			}
+		}
+	}
+	return false
+}
+
+func getCardValue(card byte) int {
+	// Numbers return their value
+	if card >= 48 && card <= 57 {
+		return int(card)
+	}
+	// For the charactes return bigger values
+
+	// T
+	if card == 84 {
+		return 58
+	}
+	// J
+	if card == 74 {
+		return 59
+	}
+	// Q
+	if card == 81 {
+		return 60
+	}
+	// K
+	if card == 75 {
+		return 61
+	}
+	// A
+	if card == 65 {
+		return 62
+	}
+	panic("could not get card value")
+}
+
+type Hand struct {
+	cards string
+	bid   int
+}
+
+func (h *Hand) getHandStength() int {
+	if h.isFiveOfAKind() {
+		return 7
+	} else if h.isFourOfAKind() {
+		return 6
+	} else if h.isFullHouse() {
+		return 5
+	} else if h.isThreeOfAKind() {
+		return 4
+	} else if h.isTwoPair() {
+		return 3
+	} else if h.isOnePair() {
+		return 2
+	} else {
+		return 1
+	}
+}
+
+func (h *Hand) isHighCard() bool {
+	cardsMap := make(map[rune]int)
+	for _, c := range h.cards {
+		cardsMap[c]++
+	}
+	if len(cardsMap) == 5 {
+		return true
+	}
+	return false
+}
+
+func (h *Hand) isOnePair() bool {
+	cardsMap := make(map[rune]int)
+	for _, c := range h.cards {
+		cardsMap[c]++
+	}
+	if len(cardsMap) == 4 {
+		for _, c := range cardsMap {
+			if c == 2 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (h *Hand) isTwoPair() bool {
+	cardsMap := make(map[rune]int)
+	for _, c := range h.cards {
+		cardsMap[c]++
+	}
+	if len(cardsMap) == 3 {
+		for _, c := range cardsMap {
+			if c == 2 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (h *Hand) isThreeOfAKind() bool {
+	cardsMap := make(map[rune]int)
+	for _, c := range h.cards {
+		cardsMap[c]++
+	}
+	if len(cardsMap) == 3 {
+		for _, c := range cardsMap {
+			if c == 3 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (h *Hand) isFullHouse() bool {
+	cardsMap := make(map[rune]int)
+	for _, c := range h.cards {
+		cardsMap[c]++
+	}
+	if len(cardsMap) == 2 {
+		for _, c := range cardsMap {
+			if c == 3 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (h *Hand) isFourOfAKind() bool {
+	cardsMap := make(map[rune]int)
+	for _, c := range h.cards {
+		cardsMap[c]++
+	}
+	if len(cardsMap) == 2 {
+		for _, c := range cardsMap {
+			if c == 4 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (h *Hand) isFiveOfAKind() bool {
+	for i := 1; i < len(h.cards); i++ {
+		if h.cards[i] != h.cards[i-1] {
+			return false
+		}
+	}
+	return true
+}
---
+++ 7_1/main_test.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSortHandsReturnCorrectOrder(t *testing.T) {
+	unsortedHands := []Hand{
+		{"ABCDE", 0},
+		{"AAAAB", 0},
+		{"AACDE", 0},
+		{"AAADE", 0},
+	}
+	sortedHands := sortHands(unsortedHands)
+
+	expectedHands := []Hand{
+		{"AAAAB", 0},
+		{"AAADE", 0},
+		{"AACDE", 0},
+		{"ABCDE", 0},
+	}
+
+	for i := range expectedHands {
+		if sortedHands[i] != expectedHands[i] {
+			t.Error("expected", expectedHands, "got", sortedHands)
+		}
+	}
+}
+
+func TestIsHighCardFalse(t *testing.T) {
+	hand := Hand{"ABACB", 0}
+	result := hand.isHighCard()
+
+	if result {
+		t.Fail()
+	}
+}
+
+func TestIsHighCardTrue(t *testing.T) {
+	hand := Hand{"ABCDE", 0}
+	result := hand.isHighCard()
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsOnePairFalse(t *testing.T) {
+	hand := Hand{"ABACB", 0}
+	result := hand.isOnePair()
+
+	if result {
+		t.Fail()
+	}
+}
+
+func TestIsOnePairTrue(t *testing.T) {
+	hand := Hand{"ABBDC", 0}
+	result := hand.isOnePair()
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsTwoPairFalse(t *testing.T) {
+	hand := Hand{"ABAAB", 0}
+	result := hand.isTwoPair()
+
+	if result {
+		t.Fail()
+	}
+}
+
+func TestIsTwoPairTrue(t *testing.T) {
+	hand := Hand{"ABBAC", 0}
+	result := hand.isTwoPair()
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsThreeOfAKindFalse(t *testing.T) {
+	hand := Hand{"ABAAB", 0}
+	result := hand.isThreeOfAKind()
+
+	if result {
+		t.Fail()
+	}
+}
+
+func TestIsThreeOfAKindTrue(t *testing.T) {
+	hand := Hand{"ABAAC", 0}
+	result := hand.isThreeOfAKind()
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsFullHouseFalse(t *testing.T) {
+	hand := Hand{"ABAAA", 0}
+	result := hand.isFullHouse()
+
+	if result {
+		t.Fail()
+	}
+}
+
+func TestIsFullHouseTrue(t *testing.T) {
+	hand := Hand{"ABAAB", 0}
+	result := hand.isFullHouse()
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsFourOfAKindFalse(t *testing.T) {
+	hand := Hand{"AAAAA", 0}
+	result := hand.isFourOfAKind()
+
+	if result {
+		t.Fail()
+	}
+}
+
+func TestIsFourOfAKindTrue(t *testing.T) {
+	hand := Hand{"AAAAB", 0}
+	result := hand.isFourOfAKind()
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsFiveOfAKindTrue(t *testing.T) {
+	hand := Hand{"AAAAA", 0}
+	result := hand.isFiveOfAKind()
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsFiveOfAKindFalse(t *testing.T) {
+	hand := Hand{"AAAAB", 0}
+	result := hand.isFiveOfAKind()
+
+	if result {
+		t.Fail()
+	}
+}
