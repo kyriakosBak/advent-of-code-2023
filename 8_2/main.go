4d61bd32899dd1ca8f067b762acd45a0746edaab
Kyriakos <kyriakos.bakirlis@gmail.com>
2024-08-09 16:07:54 +0200

    Strarting implementation of LCM

--- 8_2/main.go
+++ 8_2/main.go
@@ -28,7 +28,7 @@ func main() {
 	// Get nodes and moves
 	moves := ""
 	nodePaths := []Node{}
-	nodeMap := make(map[int]Node)
+	nodeMap := make(map[string]Node)
 	for scanner.Scan() {
 		line := scanner.Text()
 
@@ -44,60 +44,88 @@ func main() {
 		}
 
 		node := getNode(line)
-		nodeMap[node.NodeKey] = node
+		nodeMap[node.Label] = node
 		if strings.HasSuffix(node.Label, "A") {
 			fmt.Println("Found starting node", node)
 			nodePaths = append(nodePaths, node)
 		}
 	}
 
+	nodeStepsToZ := make(map[string]int)
+	for _, node := range nodePaths {
+		nodeStepsToZ[node.Label] = -1
+	}
+
 	counter := 0
-	for counter < 100000000 {
+	for counter < 100000 {
 		for _, m := range moves {
 			for i, node := range nodePaths {
 				if string(m) == "R" {
-					nodePaths[i] = nodeMap[node.RightNodeKey]
+					nodePaths[i] = nodeMap[node.RightNode]
 				} else if string(m) == "L" {
-					nodePaths[i] = nodeMap[node.LeftNodeKey]
+					nodePaths[i] = nodeMap[node.LeftNode]
 				}
-			}
 
-			counter++
+				counter++
 
-			// Check if we are at Z
-			allNodesAreZ := true
-			for _, node := range nodePaths {
-				if !strings.HasSuffix(node.Label, "Z") {
-					allNodesAreZ = false
-					break
+				if strings.HasSuffix(nodePaths[i].Label, "Z") {
+					if nodeStepsToZ[nodePaths[i].Label] == -1 {
+						nodeStepsToZ[nodePaths[i].Label] = counter
+					}
+
+					for _, steps := range nodeStepsToZ {
+						if steps == -1 {
+							continue
+						}
+					}
+
+					steps := []int{}
+					for _, step := range nodeStepsToZ {
+						steps = append(steps, step)
+					}
+					lcm := LCM(steps[0], steps[1], steps[2:]...)
+					fmt.Println(lcm)
+					fmt.Println(nodeStepsToZ)
+					fmt.Println(counter)
+					return
 				}
-			}
-
-			if allNodesAreZ {
-				fmt.Println(counter)
-				fmt.Println(nodePaths)
-				return
 			}
 		}
 	}
-}
-
-func getNodeKey(label string) int {
-	return (int(label[0]-'A')*26*26 +
-		int(label[1]-'A')*26 +
-		int(label[2]-'A'))
 }
 
 func getNode(line string) Node {
 	label := line[:3]
 	leftNode := line[7:10]
 	rightNode := line[12:15]
-	return Node{label, getNodeKey(label), getNodeKey(leftNode), getNodeKey(rightNode)}
+	return Node{label, leftNode, rightNode}
 }
 
 type Node struct {
-	Label        string
-	NodeKey      int
-	LeftNodeKey  int
-	RightNodeKey int
+	Label     string
+	LeftNode  string
+	RightNode string
+}
+
+// Copied lcm from: https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
